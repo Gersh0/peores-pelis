8772e3b8b7f44ff1164f4542a0ab3716964ed640
Gersh0 <german.rivera@eia.edu.co>
2024-09-06 11:14:06 -0500

    Refactor directores.seed.ts to generate unique IDs for each director

--- src/seed/data/directores.seed.ts
+++ src/seed/data/directores.seed.ts
@@ -31,7 +31,7 @@ export const directoresSeed: Director[] = [
         fechaNacimiento: new Date("1954-01-01"),
         nacionalidad: "Italian",
         biografia: "Claudio Fragasso is an Italian filmmaker, known for his work in the horror genre.",
-        id: "8c2e3d39-c21d-493e-9962-618e82f2f6b9",
+        id: uuidv4(),
         peoresPeliculas: []
     },
     {
@@ -39,7 +39,7 @@ export const directoresSeed: Director[] = [
         fechaNacimiento: new Date("1925-06-12"),
         nacionalidad: "American",
         biografia: "Harold P. Warren was an American writer and director, known for his cult film Manos.",
-        id: "4c2802e4-4e6c-4184-bc1e-8164d5bbbed1",
+        id: uuidv4(),
         peoresPeliculas: []
     },
     {
@@ -47,7 +47,7 @@ export const directoresSeed: Director[] = [
         fechaNacimiento: new Date("1900-01-01"),
         nacionalidad: "American",
         biografia: "Emmett Alston is an American filmmaker, known for his work in the martial arts genre.",
-        id: "b3b9ba9d-6802-42dc-ac9f-a0d2c3ea6148",
+        id: uuidv4(),
         peoresPeliculas: []
     },
     {
@@ -55,7 +55,7 @@ export const directoresSeed: Director[] = [
         fechaNacimiento: new Date("1900-01-01"),
         nacionalidad: "Korean",
         biografia: "Woo-sang Park is a Korean filmmaker, known for his work in the action genre.",
-        id: "02be7d00-a581-43b6-baef-45f08e0fad70",
+        id: uuidv4(),
         peoresPeliculas: []
     },
     {
@@ -63,7 +63,7 @@ export const directoresSeed: Director[] = [
         fechaNacimiento: new Date("1932-01-01"),
         nacionalidad: "Iranian",
         biografia: "Amir Shervan was an Iranian-American filmmaker, known for his cult action films.",
-        id: "952f2993-1f03-4416-82d7-4f46d9ea2246",
+        id: uuidv4(),
         peoresPeliculas: []
     },
     {
@@ -71,7 +71,7 @@ export const directoresSeed: Director[] = [
         fechaNacimiento: new Date("1900-01-01"),
         nacionalidad: "American",
         biografia: "David Kellogg is an American filmmaker, known for his work in the musical genre.",
-        id: "8b3b8fee-50e9-40fe-8570-b8832737f147",
+        id: uuidv4(),
         peoresPeliculas: []
     },
     {
@@ -79,7 +79,7 @@ export const directoresSeed: Director[] = [
         fechaNacimiento: new Date("1944-01-04"),
         nacionalidad: "British",
         biografia: "Roger Christian is a British filmmaker, known for his work in the science fiction genre.",
-        id: "dd22e493-1958-427a-a58c-5d0453305148",
+        id: uuidv4(),
         peoresPeliculas: []
     }
 ];
